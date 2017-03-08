f9207e0fb3e2d2e6d956c4ddfbf7d9243ac986a4
Humphrey Mugambi <mugagambi@gmail.com>
2017-03-08 18:05:43 +0300

    completed google auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule,AuthMethods,AuthProviders } from 'angularfire2'
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
+//config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBodqvCFvIKrcR5DiWxXa3KroOWK3RKp1w',
+  authDomain: 'dkut-chart-room.firebaseapp.com',
+  databaseURL: 'https://dkut-chart-room.firebaseio.com',
+  storageBucket: 'dkut-chart-room.appspot.com',
+  messagingSenderId: '275573602811'
+}
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +22,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig,{
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup
+    }),
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
